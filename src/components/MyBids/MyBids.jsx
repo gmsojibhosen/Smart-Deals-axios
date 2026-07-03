c72e1256db4ebfda3080ed2eaa16e8e2752a833c
gmsojibhosen <gmsojibhosen@gmail.com>
2026-07-03 17:58:30 +0600

    axios request interceptor and forbidden eject

--- src/components/MyBids/MyBids.jsx
+++ src/components/MyBids/MyBids.jsx
@@ -1,21 +1,32 @@
 import React, { use, useEffect, useState } from 'react';
 import { AuthContext } from '../../contexts/AuthContext';
 import Swal from 'sweetalert2';
+import useAxiosSecure from '../ProductDetails/hooks/useAxiosSecure';
 
 const MyBids = () => {
     const { user } = use(AuthContext);
     const [bids, setBids] = useState([])
+     const axiosSecure = useAxiosSecure();
 
     useEffect(() => {
-        if (user?.email) {
-            fetch(`http://localhost:3000/bids?email=${user.email}`)
-                .then(res => res.json())
-                .then(data => {
-                    console.log(data);
-                    setBids(data)
-                })
-        }
-    }, [user?.email])
+        axiosSecure.get(`/bids?email=${user.email}`)
+            .then(data => {
+            setBids(data.data)
+        })
+        
+    },[user, axiosSecure])
+
+
+    // useEffect(() => {
+    //     if (user?.email) {
+    //         fetch(`http://localhost:3000/bids?email=${user.email}`)
+    //             .then(res => res.json())
+    //             .then(data => {
+    //                 console.log(data);
+    //                 setBids(data)
+    //             })
+    //     }
+    // }, [user?.email])
 
     const handleDeleteBid = (_id) => {
         Swal.fire({
--- src/components/ProductDetails/hooks/useAxiosSecure.jsx
+++ src/components/ProductDetails/hooks/useAxiosSecure.jsx
@@ -1,16 +1,45 @@
 import axios from "axios";
 import UseAuth from "./UseAuth";
+import { useEffect } from "react";
+import { useNavigate } from "react-router";
 
 const instance = axios.create({
-    baseURL: `http://localhost:3000`
-})
+  baseURL: `http://localhost:3000`,
+});
 const useAxiosSecure = () => {
-    const {user} = UseAuth()
-    instance.interceptors.request.use((config) => {
-        config.headers.Authorization = `Bearer ${user.accessToken}`
-    return config
-    })
-    return instance
+  const { user, signOutUser } = UseAuth();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    // request interceptor
+    const requestInterceptor = instance.interceptors.request.use((config) => {
+        const token = user.accessToken
+        if (token) {
+            config.headers.Authorization = `Bearer ${token}`
+        }
+      return config;
+    });
+    // response interceptor
+    const responseInterceptor = instance.interceptors.response.use(
+      (res) => {
+        return res;
+      },
+      (err) => {
+        const status = err.status;
+        if (status === 401 || status === 403) {
+          console.log("log out the user ofr bad request");
+          signOutUser().then(() => {
+            navigate("/register");
+          });
+        }
+      },
+    );
+    return () => {
+      instance.interceptors.request.eject(requestInterceptor);
+      instance.interceptors.response.eject(responseInterceptor);
+    };
+  }, [user, signOutUser, navigate]);
+  return instance;
 };
 
 export default useAxiosSecure;
